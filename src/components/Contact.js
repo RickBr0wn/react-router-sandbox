d4872f90d57785a19f8efdc006a2587fa21bbcdb
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-06 22:58:27 +0100

    Functional (Base Scaffold)

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Contact</h1>
+        <p>Id irure nisi adipisicing tempor cupidatat aute ipsum in occaecat consequat. Irure culpa adipisicing laborum veniam ea in tempor proident eiusmod non. Nulla anim sunt veniam fugiat aliqua esse laborum dolore. Fugiat laborum incididunt incididunt laboris mollit cillum in cupidatat reprehenderit et reprehenderit fugiat culpa reprehenderit. Occaecat veniam in reprehenderit ex commodo consequat tempor quis minim magna consectetur. Exercitation dolore Lorem ipsum est dolor ad eiusmod consequat deserunt ullamco esse et. Excepteur labore pariatur occaecat laboris nisi aute nostrud incididunt tempor do excepteur. Duis id culpa quis occaecat mollit cillum ea aliqua excepteur excepteur id enim. Duis nisi irure sit eu cillum. Quis proident veniam sunt adipisicing deserunt cupidatat est irure.</p>
+      </div>
+    )
+  }
+}
+
+export default Contact
