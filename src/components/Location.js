45813f321768c6eca0342bb1369c167fdaf2aa6c
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-06 23:39:08 +0100

    Unknown?

---
+++ src/components/Location.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Location extends Component {
+  render() {
+    return (
+      <div>
+        <p id="location" className="location">Location</p>
+        <hr />
+      </div>
+    )
+  }
+}
+
+export default Location
