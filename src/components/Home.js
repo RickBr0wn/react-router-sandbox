a25b9859f411384971f4b0e29acfe51540620ffa
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-07 12:00:56 +0100

    Cleaned code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Location from './Location'
 
 export class Home extends Component {
   render() {
     return (
       <div className="container">
         <h1>Home</h1>
-        <Location />
         <p>Veniam pariatur proident cupidatat minim qui irure ea ea qui. Deserunt est culpa laboris amet do minim ipsum excepteur labore. Amet consequat aliquip elit labore sunt pariatur labore dolore in voluptate velit aliqua occaecat aute.</p>
         <p>Id irure nisi adipisicing tempor cupidatat aute ipsum in occaecat consequat. Irure culpa adipisicing laborum veniam ea in tempor proident eiusmod non. Nulla anim sunt veniam fugiat aliqua esse laborum dolore. Fugiat laborum incididunt incididunt laboris mollit cillum in cupidatat reprehenderit et reprehenderit fugiat culpa reprehenderit. Occaecat veniam in reprehenderit ex commodo consequat tempor quis minim magna consectetur. Exercitation dolore Lorem ipsum est dolor ad eiusmod consequat deserunt ullamco esse et. Excepteur labore pariatur occaecat laboris nisi aute nostrud incididunt tempor do excepteur. Duis id culpa quis occaecat mollit cillum ea aliqua excepteur excepteur id enim. Duis nisi irure sit eu cillum. Quis proident veniam sunt adipisicing deserunt cupidatat est irure.</p>
       </div>
